153013537545cfe3916e868f6d6fb76dcdd6da54
qwertyu04 <flutewrites@gmail.com>
2024-10-22 04:42:47 +0530

    Add files via upload

---
+++ SQLite/list_row.java
@@ -0,0 +1,19 @@
+package com.example.sqlitedemo;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class list_row extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.list_row);
+    }
+}
