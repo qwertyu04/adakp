951d123c30e7acc42e369afb474be7bda3f34eb3
qwertyu04 <flutewrites@gmail.com>
2024-10-22 07:13:19 +0530

    Create main.java

---
+++ video payer/main.java
@@ -0,0 +1,31 @@
+
+package com.example.videoplayer;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.MediaController;
+import android.widget.VideoView;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        VideoView videoView =(VideoView)findViewById(R.id.vdVw);
+        //Set MediaController  to enable play, pause, forward, etc options.
+        MediaController mediaController= new MediaController(this);
+        mediaController.setAnchorView(videoView);
+        //Location of Media File
+        Uri uri = Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.video1);
+        //Starting VideView By Setting MediaController and URI
+        videoView.setMediaController(mediaController);
+        videoView.setVideoURI(uri);
+        videoView.requestFocus();
+        videoView.start();
+    }
+}
+MainActivity.java
+Displaying MainActivity.java.
