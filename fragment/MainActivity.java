979142cf6801ac52251fbcca1e3c55b657bf7cfa
qwertyu04 <flutewrites@gmail.com>
2024-10-23 08:19:18 +0530

    Add files via upload

---
+++ fragment/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.fragmentdemo;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+    }
+}
